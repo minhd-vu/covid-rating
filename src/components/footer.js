d00dc48ac1b5a3ab2f994f15f7604de458141feb
Minh Vu <minhd_vu@yahoo.com>
2021-01-04 02:37:43 -0500

    update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     return (
         <Navbar expand="sm" bg="dark" variant="dark" fixed="bottom" >
             <Container>
-                <Navbar.Text>Created by <a href="https://github.com/minhd-vu/">Minh Vu</a>. Project on <a href="https://github.com/minhd-vu/assassins">GitHub</a>.</Navbar.Text>
+                <Navbar.Text>Created by <a href="https://github.com/minhd-vu/">Minh Vu</a>. Project on <a href="https://github.com/minhd-vu/covid-rating">GitHub</a>.</Navbar.Text>
             </Container>
         </Navbar>
     );
