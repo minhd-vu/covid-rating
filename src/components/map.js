63b977a3cbfa3865a1f6bc9d38ece81162a5119b
Minh Vu <minhd_vu@yahoo.com>
2021-01-16 21:45:44 -0500

    added geocoder

--- src/components/map.js
+++ src/components/map.js
@@ -1,5 +1,6 @@
-import { useState } from "react";
-import ReactMapGL, { FullscreenControl } from "react-map-gl";
+import { useState, useRef } from "react";
+import ReactMapGL, { FullscreenControl, GeolocateControl, NavigationControl, ScaleControl } from "react-map-gl";
+import Geocoder from "react-map-gl-geocoder";
 
 export default function Map() {
 	const [viewport, setViewport] = useState({
@@ -8,13 +9,30 @@ export default function Map() {
 		zoom: 12
 	});
 
+	const mapRef = useRef();
+
 	return (
 		<ReactMapGL
 			{...viewport}
+			ref={mapRef}
 			width="80vw"
 			height="80vh"
 			onViewportChange={nextViewport => setViewport(nextViewport)}
 		>
+			<div style={{ position: 'absolute', right: 0 }}>
+				<FullscreenControl container={document.querySelector('body')} />
+				<NavigationControl />
+				<GeolocateControl
+					positionOptions={{ enableHighAccuracy: true }}
+					trackUserLocation={true}
+				/>
+				<Geocoder
+					mapRef={mapRef}
+					mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+					onViewportChange={nextViewport => setViewport(nextViewport)}
+					position="top-left"
+				/>
+			</div>
 		</ReactMapGL>
 	);
 }
