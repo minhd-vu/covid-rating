0692365a0708a48e5f3754750277aa33833ab7a0
Minh Vu <minhd_vu@yahoo.com>
2021-01-16 22:08:15 -0500

    change map sizing

--- src/components/map.js
+++ src/components/map.js
@@ -1,5 +1,5 @@
 import { useState, useRef } from "react";
-import ReactMapGL, { FullscreenControl, GeolocateControl, NavigationControl, ScaleControl } from "react-map-gl";
+import MapGL, { FullscreenControl, GeolocateControl, NavigationControl, ScaleControl } from "react-map-gl";
 import Geocoder from "react-map-gl-geocoder";
 
 export default function Map() {
@@ -12,27 +12,30 @@ export default function Map() {
 	const mapRef = useRef();
 
 	return (
-		<ReactMapGL
-			{...viewport}
-			ref={mapRef}
-			width="80vw"
-			height="80vh"
-			onViewportChange={nextViewport => setViewport(nextViewport)}
-		>
-			<div style={{ position: 'absolute', right: 0 }}>
-				<FullscreenControl container={document.querySelector('body')} />
-				<NavigationControl />
-				<GeolocateControl
-					positionOptions={{ enableHighAccuracy: true }}
-					trackUserLocation={true}
-				/>
-				<Geocoder
-					mapRef={mapRef}
-					mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
-					onViewportChange={nextViewport => setViewport(nextViewport)}
-					position="top-left"
-				/>
-			</div>
-		</ReactMapGL>
+		<div style={{ height: "100vh" }}>
+			<MapGL
+				{...viewport}
+				ref={mapRef}
+				width="100%"
+				height="100%"
+				mapStyle="mapbox://styles/mapbox/navigation-preview-day-v2"
+				onViewportChange={nextViewport => setViewport(nextViewport)}
+			>
+				<div style={{ position: 'absolute', right: 0 }}>
+					<FullscreenControl container={document.querySelector('body')} />
+					<NavigationControl />
+					<GeolocateControl
+						positionOptions={{ enableHighAccuracy: true }}
+						trackUserLocation={true}
+					/>
+					<Geocoder
+						mapRef={mapRef}
+						mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+						onViewportChange={nextViewport => setViewport(nextViewport)}
+						position="top-left"
+					/>
+				</div>
+			</MapGL>
+		</div>
 	);
 }
